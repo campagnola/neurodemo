ea0d3d6c59ae2980a75753be7e2c9d38c2cc27fa
Luke Campagnola <luke.campagnola@gmail.com>
2015-08-25 15:28:59 -0400

    Channel plots mostly working

--- demo.py
+++ demo.py
@@ -44,35 +44,43 @@ class DemoWindow(QtGui.QWidget):
         self.ptree = pt.ParameterTree()
         self.splitter.addWidget(self.ptree)
         
-        self.scroll_plot = ScrollingPlot(dt=self.dt, npts=int(1.0/self.dt),
+        self.plot_splitter = QtGui.QSplitter(QtCore.Qt.Vertical)
+        self.splitter.addWidget(self.plot_splitter)
+        
+        self.vm_plot = ScrollingPlot(dt=self.dt, npts=int(1.0/self.dt),
                                          parent=self, labels={'left': ('Membrane Potential', 'V'), 
                                                               'bottom': ('Time', 's')})
-        self.scroll_plot.setYRange(-90*mV, 50*mV)
-        self.scroll_plot.setXRange(-1000*ms, 0*ms)
-        self.scroll_plot.addLine(y=self.neuron.ek)
-        self.scroll_plot.addLine(y=self.neuron.ena)
-        self.splitter.addWidget(self.scroll_plot)
+        self.vm_plot.setYRange(-90*mV, 50*mV)
+        self.vm_plot.setXRange(-1000*ms, 0*ms)
+        self.vm_plot.addLine(y=self.neuron.ek)
+        self.vm_plot.addLine(y=self.neuron.ena)
+        self.plot_splitter.addWidget(self.vm_plot)
         self.splitter.setSizes([300, 700])
 
         self.show()
         
+        self.channel_params = [
+            ChannelParameter(self.leak, 'Ileak'),
+            ChannelParameter(self.hhna, 'INa'),
+            ChannelParameter(self.hhk, 'IK'),
+            ChannelParameter(self.dexh, 'IH'),
+        ]
+        for ch in self.channel_params:
+            ch.plots_changed.connect(self.channel_plots_changed)
+        self.channel_plots = {}
         
         self.params = pt.Parameter.create(name='params', type='group', children=[
             #dict(name='Preset', type='list', values=['', 'Basic Membrane', 'Hodgkin & Huxley']),
             dict(name='Run', type='bool', value=True),
             dict(name='Speed', type='float', value=1.0, limits=[0, 1], step=1, dec=True),
             dict(name='Temp', type='float', value=self.sim.temp, suffix='C', step=1.0),
-            dict(name='Ion Channels', type='group', children=[
-                ChannelParameter(self.leak, 'Ileak'),
-                ChannelParameter(self.hhna, 'INa'),
-                ChannelParameter(self.hhk, 'IK'),
-                ChannelParameter(self.dexh, 'IH'),
-            ]),
+            dict(name='Ion Channels', type='group', children=self.channel_params),
         ])
         self.ptree.setParameters(self.params)
         self.params.sigTreeStateChanged.connect(self.params_changed)
         
         self.runner = ndemo.SimRunner(self.sim)
+        self.runner.add_request('soma.Vm', (self.neuron, 'Vm')) 
         self.runner.new_result.connect(mp.proxy(self.new_result))
         self.start()
 
@@ -92,16 +100,43 @@ class DemoWindow(QtGui.QWidget):
             #elif param is self.params.child('Preset'):
                 #self.load_preset(val)
         
+    def channel_plots_changed(self, param, name, plot):
+        key = param.name() + '.' + name
+        if plot:
+            plt = ScrollingPlot(dt=self.vm_plot.dt, npts=self.vm_plot.npts, labels={'left': param.name() + ' ' + name})
+            plt.data_key = key
+            self.channel_plots[key] = plt
+            self.runner.add_request(key, (param.channel, name))
+            
+            # add new plot to splitter and resize all accordingly
+            sizes = self.plot_splitter.sizes()
+            self.plot_splitter.addWidget(plt)
+            size = self.plot_splitter.height() / (len(sizes) + 1.)
+            r = len(sizes) / (len(sizes)+1)
+            sizes = [s * r for s in sizes] + [size]
+            self.plot_splitter.setSizes(sizes)
+        else:
+            plt = self.channel_plots.pop(key)
+            self.runner.remove_request(key)
+            #self.plot_splitter.removeWidget(plt)
+            plt.setParent(None)
+            plt.close()
+        
     def start(self):
         self.runner.start(blocksize=100)
         
     def stop(self):
         self.runner.stop()
         
-    def new_result(self, res):
-        self.last_result = res
-        vm = res['soma', 'Vm'][1:]
-        self.scroll_plot.append(vm)
+    def new_result(self, result):
+        self.last_result = result
+        vm = result['soma.Vm'][1:]
+        self.vm_plot.append(vm)
+        
+        for k, plt in self.channel_plots.items():
+            if k not in result:
+                continue
+            plt.append(result[k])
 
     def load_preset(self, preset):
         if preset == 'Basic Membrane':
@@ -114,13 +149,24 @@ class DemoWindow(QtGui.QWidget):
 
 
 class ChannelParameter(pt.parameterTypes.SimpleParameter):
+    
+    # emitted when a plot should be shown or hidden
+    plots_changed = QtCore.Signal(object, object, object)  # self, param, value
+    
     def __init__(self, channel, name):
         self.channel = channel
-        pt.parameterTypes.SimpleParameter.__init__(self, name=name, type='bool', 
-                                                   value=channel.enabled, children=[
+        ch_params = [
             dict(name='ḡ', type='float', value=channel.gbar*cm**2, suffix='S/cm²', siPrefix=True, step=0.1, dec=True),
             dict(name='Erev', type='float', value=channel.erev, suffix='V', siPrefix=True, step=5*mV),
-        ])
+            dict(name='Plot I', type='bool', value=False),
+            dict(name='Plot G', type='bool', value=False),
+            dict(name='Plot OP', type='bool', value=False),
+        ]
+        for sv in channel.state_vars:
+            ch_params.append(dict(name='Plot ' + sv, type='bool', value=False))
+        
+        pt.parameterTypes.SimpleParameter.__init__(self, name=name, type='bool', 
+                                                   value=channel.enabled, children=ch_params)
         self.sigTreeStateChanged.connect(self.treeChange)
         
     def treeChange(self, root, changes):
@@ -133,6 +179,8 @@ class ChannelParameter(pt.parameterTypes.SimpleParameter):
                 self.channel.gbar = val/cm**2
             elif param is self.child('Erev'):
                 self.channel.erev = val
+            elif param.name().startswith('Plot'):
+                self.plots_changed.emit(self, param.name()[5:], val)
 
 
 class ScrollingPlot(pg.PlotWidget):
--- neurodemo/runner.py
+++ neurodemo/runner.py
@@ -10,8 +10,15 @@ class SimRunner(QtCore.QObject):
         QtCore.QObject.__init__(self)
         self.sim = sim
         self.speed = 1.0
+        self.requests = {}
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.run_once)
+        
+    def add_request(self, key, req):
+        self.requests[key] = req
+
+    def remove_request(self, key):
+        self.requests.pop(key)
         
     def start(self, blocksize=500, **kwds):
         self.blocksize = blocksize
@@ -23,8 +30,24 @@ class SimRunner(QtCore.QObject):
         
     def run_once(self):
         blocksize = int(max(2, self.blocksize * self.speed))
-        res = self.sim.run(blocksize, **self.run_args)
-        self.new_result.emit(res)
+        result = self.sim.run(blocksize, **self.run_args)
+        
+        ret = {}
+        for key, req in self.requests.items():
+            if isinstance(req, tuple):
+                if req[1] == 'I':
+                    data = req[0].current(result)
+                elif req[1] == 'G':
+                    data = req[0].conductance(result)
+                elif req[1] == 'OP':
+                    data = req[0].open_probability(result)
+                else:
+                    data = result[req]
+            else:
+                data = result[req]
+            ret[key] = data
+        
+        self.new_result.emit(ret)
         
     def set_speed(self, speed):
         self.speed = speed
