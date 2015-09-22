68e9fa192e071b4ec5888a63c46f534ee0be35b2
Luke Campagnola <luke.campagnola@gmail.com>
2015-09-22 17:16:52 -0400

    Added more analyzers

--- neurodemo/traceanalyzer.py
+++ neurodemo/traceanalyzer.py
@@ -92,12 +92,13 @@ class TraceAnalyzerParameter(pt.parameterTypes.GroupParameter):
     need_update = QtCore.Signal(object)  # self
 
     def __init__(self, **kwds):
-        kwds.update({'removable': True, 'renamable': True})
+        kwds.update({'removable': True, 'renamable': False})
         childs = [
             dict(name='Input', type='list', values=kwds.pop('inputs')),
-            dict(name='Type', type='list', value=kwds.pop('analysis_type'), values=['mean', 'min', 'max']),
+            dict(name='Type', type='list', value=kwds.pop('analysis_type'), values=['mean', 'min', 'max', 'exp tau', 'spike count', 'spike latency']),
             dict(name='Start', type='float', value=0, suffix='s', siPrefix=True, step=5e-3),
             dict(name='End', type='float', value=10e-3, suffix='s', siPrefix=True, step=5e-3),
+            dict(name='Threshold', type='float', value=-30e-3, suffix='V', siPrefix=True, step=5e-3, visible=False),
         ]
         kwds['children'] = childs + kwds.get('children', [])
         
@@ -117,8 +118,11 @@ class TraceAnalyzerParameter(pt.parameterTypes.GroupParameter):
                     self.rgn.setRegion([self['Start'], self['End']])
                 finally:
                     self.rgn.sigRegionChanged.connect(self.region_changed)
-            else:
-                self.need_update.emit(self)
+            elif param is self.child('Type'):
+                needs_threshold = val in ['spike count', 'spike latency']
+                self.child('Threshold').setOpts(visible=needs_threshold)
+            self.setName(self['Input'] + ': ' + self['Type'], autoIncrement=True)
+            self.need_update.emit(self)
         
     def region_changed(self):
         start, end = self.rgn.getRegion()
@@ -139,6 +143,7 @@ class TraceAnalyzerParameter(pt.parameterTypes.GroupParameter):
         i1 = int(self['Start'] / dt)
         i2 = int(self['End'] / dt)
         data = data[self['Input']][i1:i2]
+        t = t[i1:i2]
         typ = self['Type']
         if typ == 'mean':
             return data.mean()
@@ -146,21 +151,54 @@ class TraceAnalyzerParameter(pt.parameterTypes.GroupParameter):
             return data.min()
         elif typ == 'max':
             return data.max()
+        elif typ.startswith('spike'):
+            spikes = np.argwhere((data[1:] > self['Threshold']) & (data[:-1] < self['Threshold']))[:,0]
+            if typ == 'spike count':
+                return len(spikes)
+            elif typ == 'spike latency':
+                if len(spikes) == 0:
+                    return np.nan
+                else:
+                    return spikes[0] * dt
+        elif typ == 'exp tau':
+            return self.measure_tau(data, t)
+            
+    def measure_tau(self, data, t):
+        from scipy.optimize import curve_fit
+        dt = t[1] - t[0]
+        def expfn(x, yoff, amp, tau):
+            return yoff + amp * np.exp(-x / tau)
+        guess = (data[-1], data[0] - data[-1], t[-1] - t[0])
+        fit = curve_fit(expfn, t-t[0], data, guess)
+        return fit[0][2]
+            
         
 
 class EvalPlotter(QtGui.QWidget):
     def __init__(self):
         QtGui.QWidget.__init__(self)
         self.layout = QtGui.QGridLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(self.layout)
-        self.xCode = QtGui.QLineEdit('cmd')
-        self.yCode = QtGui.QLineEdit()
+        self.x_code = QtGui.QLineEdit('cmd')
+        self.y_code = QtGui.QLineEdit()
         self.plot = pg.PlotWidget()
-        self.layout.addWidget(self.xCode, 0, 0)
-        self.layout.addWidget(self.yCode, 0, 1)
+        self.layout.addWidget(self.x_code, 0, 0)
+        self.layout.addWidget(self.y_code, 0, 1)
         self.layout.addWidget(self.plot, 1, 0, 1, 2)
-        self.xCode.editingFinished.connect(self.replot)
-        self.yCode.editingFinished.connect(self.replot)
+        self.x_code.editingFinished.connect(self.replot)
+        self.y_code.editingFinished.connect(self.replot)
+
+        self.hold_plot_btn = QtGui.QPushButton('Hold Plot')
+        self.layout.addWidget(self.hold_plot_btn, 2, 0)
+        self.clear_plot_btn = QtGui.QPushButton('Clear Plot')
+        self.layout.addWidget(self.clear_plot_btn, 2, 1)
+        
+        self.held_plots = []
+        self.last_curve = None
+        self.held_index = 0
+        self.hold_plot_btn.clicked.connect(self.hold_plot)
+        self.clear_plot_btn.clicked.connect(self.clear_plot)
         
     def update_data(self, data):
         self.data = data
@@ -170,9 +208,9 @@ class EvalPlotter(QtGui.QWidget):
         data = self.data
         ns = {}
         for k in data.dtype.names:
-            ns[k] = data[k]
-        xcode = str(self.xCode.text())
-        ycode = str(self.yCode.text())
+            ns[k.replace(' ', '_')] = data[k]
+        xcode = str(self.x_code.text())
+        ycode = str(self.y_code.text())
         if xcode == '' or ycode == '':
             return
         
@@ -180,21 +218,34 @@ class EvalPlotter(QtGui.QWidget):
             x = eval(xcode, ns)
         except:
             pg.debug.printExc('Error evaluating plot x values:')
-            self.xCode.setStyleSheet("QLineEdit { border: 2px solid red; }")
+            self.x_code.setStyleSheet("QLineEdit { border: 2px solid red; }")
             return
         else:
-            self.xCode.setStyleSheet("")
+            self.x_code.setStyleSheet("")
             
         try:
             y = eval(ycode, ns)
         except:
             pg.debug.printExc('Error evaluating plot y values:')
-            self.yCode.setStyleSheet("QLineEdit { border: 2px solid red; }")
+            self.y_code.setStyleSheet("QLineEdit { border: 2px solid red; }")
             return
         else:
-            self.yCode.setStyleSheet("")
+            self.y_code.setStyleSheet("")
             
-            
-        self.plot.plot(x, y, symbol='o', clear=True)
+        if self.last_curve is None:
+            self.last_curve = self.plot.plot(x, y, symbol='o', symbolBrush=(self.held_index, 10))
+        else:
+            self.last_curve.setData(x, y)
         
-    
+    def hold_plot(self):
+        if self.last_curve is None:
+            return
+        self.held_plots.append(self.last_curve)
+        self.held_index += 1
+        self.last_curve = None
+
+    def clear_plot(self):
+        self.held_plots = []
+        self.held_index = 0
+        self.plot.clear()
+        self.replot()
