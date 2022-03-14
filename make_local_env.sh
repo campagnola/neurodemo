8010c889faf01999644d034d2d9ed04dafa6ec59
Paul Manis <pmanis@med.unc.edu>
2022-03-14 17:56:53 -0400

    adding env build script (linux/osx)

---
+++ make_local_env.sh
@@ -0,0 +1,29 @@
+set -e # force failure if anyting fails in script - ensuring completion
+set -o errexit
+ENVNAME="neurodemo_venv"
+if [ -d $ENVNAME ]
+then
+    echo "Removing previous environment: $ENVNAME"
+    set +e
+    rsync -aR --remove-source-files $ENVNAME ~/.Trash/ || exit 1
+    set -e
+    rm -R $ENVNAME
+else
+    echo "No previous environment to remove."
+fi
+python3.9 -m venv $ENVNAME || exit 1
+source $ENVNAME/bin/activate || exit 1
+
+pip3 install --upgrade pip  # be sure pip is up to date in the new env.
+pip3 install wheel  # seems to be missing (note singular)
+pip3 install cython
+# # if requirements.txt is not present, create:
+# # pip install pipreqs
+# # pipreqs
+#
+# #Then:
+#
+pip3 install -r requirements_local.txt
+source $ENVNAME/bin/activate
+python --version
+python setup.py develop
