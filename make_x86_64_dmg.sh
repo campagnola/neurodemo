b1b653400f3174a8076e365f9c10b129451e7996
Paul Manis <pmanis@med.unc.edu>
2022-08-11 18:19:01 -0400

    fix icon size for x86 version

---
+++ make_x86_64_dmg.sh
@@ -0,0 +1,23 @@
+# make Apple Silicon app
+#
+# rm -rf build dist
+# python setup_x86_64.py py2app
+mkdir -p dist/dmg
+cp -r "dist/demo.app" dist/dmg
+# If the DMG already exists, delete it.
+test -f "dist/demo.dmg" && rm "dist/demo.dmg"
+# get create-dmg from homebrew
+create-dmg \
+--volname "demo" \
+--volicon "icon.icns" \
+--window-pos 200 120 \
+--window-size 600 300 \
+--icon-size 32 \
+--icon "demo.app" 32 32 \
+--hide-extension "demo.app" \
+--app-drop-link 425 120 \
+"dist/demo.dmg" \
+"dist/dmg/"
+
+#hdiutil create -srcFolder dist -o dmg/neurodemo
+
