c2421401d2c5cbd23237b5c3e189d030264a40e8
Paul Manis <pmanis@med.unc.edu>
2022-08-11 15:38:40 -0400

    clean up env maker

--- make_local_env.sh
+++ make_local_env.sh
@@ -20,12 +20,7 @@ python3 -m pip install --upgrade pip
 pip3 install --upgrade pip  # be sure pip is up to date in the new env.
 pip3 install wheel  # seems to be missing (note singular)
 pip3 install cython
-# # if requirements.txt is not present, create:
-# # pip install pipreqs
-# # pipreqs
-#
-# #Then:
-#
+
 pip3 install -r requirements_local.txt
 source $ENVNAME/bin/activate
 python --version
