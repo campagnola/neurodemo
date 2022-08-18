1b9cf7706d1f8a7aa5fd0cfb152213244ba4c731
Paul B. Manis <pmanis@med.unc.edu>
2022-08-18 16:05:55 -0400

    Windows stuff

--- make_local_env_win.sh
+++ make_local_env_win.sh
@@ -7,24 +7,25 @@ then
     set +e
     # rsync -aR --remove-source-files $ENVNAME ~/.Trash/ || exit 1
     set -e
-    rm -R $ENVNAME
+    rm -Rf $ENVNAME 
 else
     echo "No previous environment to remove."
 fi
 # python3.9 -m venv $ENVNAME || exit 1
-py -m venv $ENVNAME || exit 1]
+py -m venv $ENVNAME || exit 1
 # source $ENVNAME/bin/activate || exit 1
 source $ENVNAME/Scripts/Activate
-# pip3 install --upgrade pip  # be sure pip is up to date in the new env.
-pip3 install wheel  # seems to be missing (note singular)
-pip3 install cython
+# pip3 install --upgrade pip  # be sure pip is up to date in the new env.py
+
+python -m pip install wheel  # seems to be missing (note singular)
+python -m pip install cython
 # # if requirements.txt is not present, create:
 # # pip install pipreqs
 # # pipreqs
 #
 # #Then:
 #
-pip3 install -r requirements_local.txt
+python -m pip install -r requirements_local.txt
 source $ENVNAME/Scripts/Activate
 python --version
 python setup_local.py develop
