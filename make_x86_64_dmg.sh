1312ae152af64538c7f159f85438895585439a6d
Paul Manis <pmanis@med.unc.edu>
2022-08-29 17:08:50 -0400

    fixing x86_64 dmg builder (add a few delays)

--- make_x86_64_dmg.sh
+++ make_x86_64_dmg.sh
@@ -1,11 +1,17 @@
+# If the DMG already exists, delete it.
+test -f "dist/demo_x86_64.dmg" && rm "dist/demo_x86_64.dmg"
+sleep 3
+test -f "dist/demo.dmg" && rm "dist/demo.dmg"
+
 # make x86_64 app
 #
-rm -rf build dist
-python setup_x86_64.py py2app
-mkdir -p dist/dmg
+# rm -rf build dist
+# python setup_x86_64.py py2app
+# mkdir -p dist/dmg
+sleep 3
+
 cp -r "dist/demo.app" dist/dmg
-# If the DMG already exists, delete it.
-test -f "dist/demo.dmg" && rm "dist/demo.dmg"
+
 # get create-dmg from homebrew
 create-dmg \
 --volname "demo" \
@@ -16,6 +22,7 @@ create-dmg \
 --icon "demo.app" 100 100 \
 --hide-extension "demo.app" \
 --app-drop-link 250 100 \
+--no-internet-enable \
 "dist/demo_x86_64.dmg" \
 "dist/dmg/"
 
