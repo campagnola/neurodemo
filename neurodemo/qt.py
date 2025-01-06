abb34301ac42abf1037d5222cb81d6dc4f5bf504
Thomas Jhou <tomjhou@gmail.com>
2025-01-06 15:49:18 -0500

    PyQt wrapper now works with PyQt5. Removed redundant imports.

--- neurodemo/qt.py
+++ neurodemo/qt.py
@@ -1,11 +1,16 @@
 import importlib
 import pyqtgraph as pg
 
-Qt = importlib.__import__(pg.Qt.QT_LIB + '.QtSvgWidgets')
+if pg.Qt.QT_LIB == 'PyQt6':
+    Qt = importlib.__import__(pg.Qt.QT_LIB + '.QtSvgWidgets')
+    mod_list = [pg.Qt, pg.Qt.QtCore, pg.Qt.QtGui, pg.Qt.QtWidgets, pg.Qt.QtSvg, Qt.QtSvgWidgets]
+else:
+    # PyQt5 doesn't need the extra import
+    mod_list = [pg.Qt, pg.Qt.QtCore, pg.Qt.QtGui, pg.Qt.QtWidgets, pg.Qt.QtSvg]
 
 # make one large namespace containing everything; pyqtgraph handles translation
 # between different Qt versions
-for mod in [pg.Qt, pg.Qt.QtCore, pg.Qt.QtGui, pg.Qt.QtWidgets, pg.Qt.QtSvg, Qt.QtSvgWidgets]:
+for mod in mod_list:
     ns = mod.__dict__.copy()
     # don't copy special variables like __name__, __file__, etc.
     for k in list(ns.keys()):
